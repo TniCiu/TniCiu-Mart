b6291228602328b53a61b508da96905cf3359a01
TniCiu <tindoris29@gmail.com>
2025-06-18 14:58:36 +0700

    feat: Init setting mysql connection to server

--- TniCiu-Mart-Server/src/config/environment.js
+++ TniCiu-Mart-Server/src/config/environment.js
@@ -1,8 +1,13 @@
-import "dotenv/config"
+import "dotenv/config";
 
 export const env = {
   APP_HOST: process.env.APP_HOST,
   APP_PORT: process.env.APP_PORT,
   BUILD_MODE: process.env.BUILD_MODE,
   AUTHOR: process.env.AUTHOR,
+
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME,
 };
---
+++ TniCiu-Mart-Server/src/config/mysql.js
@@ -0,0 +1,19 @@
+import { env } from "./environment";
+import mysql from "mysql2/promise";
+
+let connection;
+
+export const CONNECT_DB = async () => {
+  connection = await mysql.createConnection({
+    host: env.DB_HOST,
+    user: env.DB_USER,
+    password: env.DB_PASSWORD,
+    database: env.DB_NAME,
+  });
+};
+
+export const CLOSE_DB = async () => {
+  if (connection) {
+    await connection.end();
+  }
+};
